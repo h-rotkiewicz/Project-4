f1414a4ec899fda57d0949c8907694872ef059ba
Hubert Rotkiewicz <s193421@student.pg.edu.pl>
2023-06-04 14:25:08 +0200

    Double buffering support

--- Eng.h
+++ Eng.h
@@ -1,13 +1,17 @@
+#define Double_Buffering
 #include <X11/Xlib.h>
+#include <X11/extensions/Xdbe.h> // for double buffering
+#include <X11/Xutil.h>
 #include <memory>
 #include <iostream>
 #include <string>
 #include <utility>
 #include <array>
+#include <vector>
 
 class IDrawable{
     protected:
-        virtual void draw(Display *disp, Window const & win, GC const &gc) const = 0;
+        virtual void draw(Display *disp, XdbeBackBuffer, GC const &gc) const = 0;
         friend class DisplayManager; // only draw objects from DisplayManager 
     public:
         virtual ~IDrawable() = default;
@@ -20,10 +24,9 @@ class Floor : public IDrawable{
         int x_;
         int y_;
 
-        void draw(Display *disp, Window const &win, GC const &gc) const override  {
+        void draw(Display *disp, long unsigned back_buff, GC const &gc) const override  {
             XSetForeground(disp, gc, 0x0000FF);
-            XFillRectangle(disp, win, gc, x_, y_, lenght_, width_);
-            XFlush(disp);
+            XFillRectangle(disp, back_buff, gc, x_, y_, lenght_, width_);
         }
 
     public:
@@ -37,10 +40,8 @@ class Elevator : public IDrawable{
         int x_;
         int y_;
 
-        void draw(Display *disp, Window const &win, GC const &gc) const override {
-            //TODO:change to actual draw function
-            XFillRectangle(disp, win, gc, x_, y_, width_, height_);
-            XFlush(disp);
+        void draw(Display *disp, long unsigned back_buff, GC const &gc) const override {
+            
         }
 
     public:
@@ -53,6 +54,7 @@ inline auto WindowDeleter = [](Display* disp) noexcept {
         XCloseDisplay(disp);
 };
 
+
 //Singleton class for managing X11 display and window
 class DisplayManager {
     static constexpr auto window_width_ = 600; 
@@ -62,19 +64,41 @@ class DisplayManager {
     const char *window_title_ = "Project 4";
     std::unique_ptr<Display, decltype(WindowDeleter)> disp_ = {XOpenDisplay(NULL), WindowDeleter};
     Window window_;
+    XdbeBackBuffer back_buffer_;
+    long unsigned *chosen_buffer_;
     XEvent event_;
     GC gc_;
     int screen_;
-
+    int minor_;
+    int major_;
+    XdbeSwapInfo swap_info_;
+    bool double_buffering_ = true;
+    
     DisplayManager(){ 
         if(!disp_) {
             throw std::runtime_error("Failed to open X11 display");
         }
         screen_ = DefaultScreen(disp_.get());
-        window_ = XCreateSimpleWindow(disp_.get(), RootWindow(disp_.get(), screen_), 0,0 ,window_width_ ,window_height_ , border_width_, BlackPixel(disp_.get(), screen_), WhitePixel(disp_.get(), screen_));
+        window_ = XCreateSimpleWindow(disp_.get(), RootWindow(disp_.get(), screen_), 0,0 ,window_width_ ,window_height_ , 
+                border_width_, BlackPixel(disp_.get(), screen_), WhitePixel(disp_.get(), screen_));
         XSelectInput(disp_.get(), window_, ExposureMask | KeyPressMask);
         XMapWindow(disp_.get(), window_);
         gc_ = XCreateGC(disp_.get(), window_, 0, 0);
+
+        if(!XdbeQueryExtension(disp_.get() ,&major_, &minor_)) {
+            std::cerr << "Double buffering not supported\n";
+            std::cerr << "Proceeding with single buffer\n";
+            std::cerr << "Program may be slow and flicker\n";
+            double_buffering_ = false;
+            chosen_buffer_ = &window_;
+        }
+        else {
+            chosen_buffer_ = &back_buffer_;
+            swap_info_.swap_window = window_;
+            //For ref see https://manpages.ubuntu.com/manpages/trusty/man3/XdbeSwapBuffers.3.html
+            swap_info_.swap_action = XdbeUndefined; 
+            back_buffer_ = XdbeAllocateBackBufferName(disp_.get(), swap_info_.swap_window, swap_info_.swap_action);
+        }
     }
 
     public:
@@ -94,15 +118,20 @@ class DisplayManager {
     ~DisplayManager(){
         XDestroyWindow(disp_.get(), window_);
     }
-    
-// `cont` should be a container of IDrawable objects (e.g std::vector<std::shared_ptr<IDrawable>>)
+
+    // `cont` should be a container of IDrawable objects (e.g std::vector<std::shared_ptr<IDrawable>>)
+    //  the objects will be drawn in the order they are stored in the container
+    //  auto is used to allow flexibility in the type of container used
     void event_loop(auto &cont){
         while(1) {
             XNextEvent(disp_.get(), &event_);
             if(event_.type == Expose) {
                 // TODO: implement a double buffer solution
-                for(auto i : cont) {
-                    i->draw(disp_.get(), window_, gc_);
+                for(auto &i : cont) {
+                    i->draw(disp_.get(), *chosen_buffer_, gc_);
+                }
+                if(double_buffering_) {
+                    XdbeSwapBuffers(disp_.get(), &swap_info_, 1);
                 }
             }
             if(event_.type == KeyPress) {
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ constexpr int floor_lenght = 200;
  * 2. Add elevator and floor class that inherit from drawable
  * 3. Add Event class that handles events with an interface
  * 4. and more ...
+ * 5. change auto in function parameters to actual types
  * */
 
 void Create_Floors(auto &cont, int window_width, int window_height, int number_of_floors){
@@ -15,22 +16,29 @@ void Create_Floors(auto &cont, int window_width, int window_height, int number_o
     //left
     int k = 1;
     for(int i = 0; i < number_of_floors;k++, i++){
-        cont[i] = new Floor(floor_lenght, floor_width, 0, k*floor_spaceing);
+        cont[i] = std::make_unique<Floor>(Floor(floor_lenght, floor_width, 0, k*floor_spaceing));
     }
 
     //right
     for(int i = number_of_floors/2, k=1; i < number_of_floors; k++,i++){
-        cont[i] = new Floor(floor_lenght, floor_width, window_width-floor_lenght, k*floor_spaceing);
+        cont[i] = std::make_unique<Floor>(Floor(floor_lenght,floor_width, window_width-floor_lenght, k*floor_spaceing));
     }
 }
 
 int main() {
+    try{
     DisplayManager& disp = DisplayManager::GetInstance();
     const int number_of_floors = 12;
-    std::array<IDrawable*,number_of_floors> cont;
+    std::vector<std::unique_ptr<IDrawable>> cont;
+    cont.resize(number_of_floors);
     Create_Floors(cont, disp.get_width(), disp.get_height(),number_of_floors);
     disp.event_loop(cont);
     return 0;
+    }
+    catch(std::exception &e){
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
  }
 
 
