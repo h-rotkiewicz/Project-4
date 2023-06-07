ef9696ca43acd837f49a3a5b6154e92d3e511e44
Oskar N <s193221@student.pg.edu.pl>
2023-06-07 11:12:43 +0200

    Better elevator model

--- Actors.h
+++ Actors.h
@@ -8,14 +8,14 @@
 
 class IDrawable{
     protected:
-        uint lenght_;
+        uint length_;
         uint width_;
         int x_;
         int y_;
         virtual void draw(Display *disp, XdbeBackBuffer, GC const &gc) const = 0;
         friend class DisplayManager; // only draw objects from DisplayManager 
     public:
-        IDrawable(uint lenght, uint width,  int x, int y): lenght_(lenght), width_(width),  x_(x), y_(y) {}
+        IDrawable(uint length, uint width,  int x, int y): length_(length), width_(width),  x_(x), y_(y) {}
         int get_x() const {
             return x_;
         }
@@ -31,29 +31,45 @@ class IMoveable : public IDrawable{
         friend class DisplayManager; // only draw objects from DisplayManager 
     public:
         virtual ~IMoveable() = default;
-        IMoveable(uint lenght, uint width,  int x, int y): IDrawable(lenght, width, x, y) {}
+        IMoveable(uint length, uint width,  int x, int y): IDrawable(length, width, x, y) {}
 };
 
 class Floor : public IDrawable{
     private:
         void draw(Display *disp, long unsigned back_buff, GC const &gc) const override  {
             XSetForeground(disp, gc, 0x0000FF);
-            XFillRectangle(disp, back_buff, gc, x_, y_, lenght_, width_);
+            XFillRectangle(disp, back_buff, gc, x_, y_, length_, width_);
         }
 
     public:
-        Floor(uint lenght, uint width,  int x, int y): IDrawable(lenght, width, x, y) {}
+        Floor(uint length, uint width,  int x, int y): IDrawable(length, width, x, y) {}
 };
 
 class Elevator : public IMoveable{
     private:
+    const int wall_thickness=10;
         void draw(Display *disp, long unsigned back_buff, GC const &gc) const override {
-            XSetForeground(disp, gc, 0x00FF00);
-            XFillRectangle(disp, back_buff, gc, x_, y_, lenght_, width_);
+            XSetForeground(disp, gc, 0xDCDCDC);
+            XFillRectangle(disp, back_buff, gc, x_, y_, length_, width_); //back wall   
+            XSetForeground(disp, gc, 0x00FFFF);
+            XFillRectangle(disp, back_buff, gc, x_, y_, length_, wall_thickness); //celling 
+            XFillRectangle(disp, back_buff, gc, x_, y_+width_, length_, wall_thickness); //floor
+            XFillRectangle(disp, back_buff, gc, x_, y_+wall_thickness, wall_thickness, width_-wall_thickness); // left wall
+            XFillRectangle(disp, back_buff, gc, x_+length_-wall_thickness, y_+wall_thickness, wall_thickness, width_-wall_thickness); //right wall
         }
         void move() override {}
     public:
-        Elevator(uint lenght, uint width,  int x, int y): IMoveable(lenght, width, x, y) {}
+        Elevator(uint length, uint width,  int x, int y): IMoveable(length, width, x, y) {}
+};
+
+class Human : public IMoveable{
+    private:
+        void draw(Display *disp, long unsigned back_buff, GC const &gc) const override {
+            
+        }
+        void move() override {}
+    public:
+        Human(uint length, uint width,  int x, int y): IMoveable(length, width, x, y) {}
 };
 
 //since 2 objects have nearly the same constructor, we can use a template to avoid code duplication
@@ -74,7 +90,7 @@ class ObjectObserver{
     std::vector<std::unique_ptr<IDrawable>> container_;
     std::vector<std::unique_ptr<IMoveable>> moveable_container_;
     const int floor_width = 20;
-    const int floor_lenght = 200;
+    const int floor_length = 200;
     const int elevator_width = 200;
     const int elevator_height = 120;
 
@@ -99,21 +115,21 @@ class ObjectObserver{
     }
 
     void create_floors(int window_width, int window_height, int number_of_floors){
-        int floor_spaceing = window_height/(number_of_floors);
+        int floor_spacing = window_height/(number_of_floors);
 
         //left
         int k = 1;
         for(int i = 0; i < number_of_floors;k++, i++){
-            add_object<Floor>(floor_lenght,floor_width,0, k*floor_spaceing);
+            add_object<Floor>(floor_length,floor_width,0, k*floor_spacing-floor_width);
         }
 
         //right
         for(int i = number_of_floors/2, k=1; i < number_of_floors; k++,i++){
-            add_object<Floor>(floor_lenght, floor_width,window_width-floor_lenght, k*floor_spaceing+floor_spaceing/2);
+            add_object<Floor>(floor_length, floor_width,window_width-floor_length, k*floor_spacing+floor_spacing/2-floor_width);
         }
     }
 
     void create_elevator(int window_width, int window_height){
-        add_object<Elevator>(elevator_width, elevator_height,floor_lenght, window_height-elevator_height);
+        add_object<Elevator>(elevator_width, elevator_height,floor_length, window_height-elevator_height-floor_width);
     }
 };
